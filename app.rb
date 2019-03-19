a57929cd2ad9bca97c93ba3f3e6c4016ebd8d254
Laura Messa Travin <laura.travin@gmail.com>
2019-03-19 21:46:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get 'reverseaname/:name'
+  get '/reverseaname/:name'
     #  @name= params[:name].reverse!
     #  puts @name
   end
