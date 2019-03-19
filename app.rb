c8596e06b6f61445950213fa24f4ca84ab817721
Laura Messa Travin <laura.travin@gmail.com>
2019-03-19 23:08:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @num1= params[:number1].to_i
     @num2= params[:number2].to_i
-
+binding.pry
    @answer=  @num1.public_send(params[:operation].to_sym, @num2)
   end
 
