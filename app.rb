9dc63f97e0965b3841b4f5250e4d30f126ddcd34
Laura Messa Travin <laura.travin@gmail.com>
2019-03-19 22:57:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,9 @@ class App < Sinatra::Base
     erb :"say"
   end
   get '/:operation/:number1/:number2' do
-    @result= params[number1].to_i params[:operation] params[:number2].to_i
+    @num1= params[number1].to_i 
+    @ope= params[:operation] 
+    @num2= params[:number2].to_i
   end
 
 end
